5d74e9e818aca5ca409332cf17f4acf35c206628
Colin Doak <cdoak@latitudegeo.com>
2023-01-26 11:15:17 -0800

    feat: add select

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 // Activities will be re-exported from this file.
 export { default as CreateSketch } from "./activities/CreateSketch";
 
-export {default as UpdateSketch} from "./activities/UpdateSketch";
+export { default as UpdateSketch } from "./activities/UpdateSketch";
+
+export { default as SelectGraphic } from "./activities/SelectGraphic";
