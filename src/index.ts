f78a690ade22051622e97bc6b95fec895526d27c
Ryan Cooney <rcooney@users.noreply.github.com>
2023-02-06 13:41:08 -0800

    feat: add Create Layer and Add Layer To Map activities (#7)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 // Activities will be re-exported from this file.
+export { default as AddLayerToMap } from "./activities/AddLayerToMap";
+export { default as CreateLayerFromArcGisServerUrl } from "./activities/CreateLayerFromArcGisServerUrl";
+export { default as CreateLayerFromPortalItem } from "./activities/CreateLayerFromPortalItem";
 export { default as CreateSketch } from "./activities/CreateSketch";
-
 export {default as UpdateSketch} from "./activities/UpdateSketch";
