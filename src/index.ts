040398eed7648219907006839d86e97f9902894d
Colin Doak <cdoak@latitudegeo.com>
2023-02-13 14:14:29 -0800

    feat: Add Measure tools

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,7 @@ export { default as CreateLayerFromArcGisServerUrl } from "./activities/CreateLa
 export { default as CreateLayerFromPortalItem } from "./activities/CreateLayerFromPortalItem";
 export { default as CreateSketch } from "./activities/CreateSketch";
 export { default as SelectGraphics } from "./activities/SelectGraphics";
-export { default as UpdateSketch } from "./activities/UpdateSketch";
+export { default as UpdateSketch  } from "./activities/UpdateSketch";
+export { default as CreateMeasurement2D } from "./activities/CreateMeasurement2D";
+export { default as RemoveMeasurement } from "./activities/RemoveMeasurement";
+
