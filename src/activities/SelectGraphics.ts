bcb988ed843e28bbaeba150a65b698c3f3bfd763
Colin Doak <cdoak@latitudegeo.com>
2023-02-15 11:12:55 -0800

    fix: - removed nested async / then from events - switched to highlight rather than tool tip for selection feedback - cleaned up handles in finally statement

--- src/activities/SelectGraphics.ts
+++ src/activities/SelectGraphics.ts
@@ -36,64 +36,74 @@ export default class SelectGraphics implements IActivityHandler {
 
     async execute(inputs: SelectGraphicsInputs, context: IActivityContext,
         type: typeof MapProvider): Promise<SelectGraphicsOutputs> {
+
+        let pointerHandle: __esri.Handle | undefined;
+        let clickHandle: __esri.Handle | undefined;
+        let highlightHandle: __esri.Handle | undefined;
+        let keyDown;
+        let graphics: Graphic[] | undefined;
         const { layer } = inputs;
         const mapProvider = type.create();
+
         await mapProvider.load();
-        let pointerHandle: IHandle;
-        let clickHandle: IHandle;
-        let keyDown;
         if (!mapProvider.view) {
-            throw new Error("view is required");
+            throw new Error("map view not found");
         }
         if (!layer) {
             throw new Error("layer is required");
         }
         const mapView = mapProvider.view as MapView;
+        const layerView = await mapView.whenLayerView(layer);
 
-        const graphics: Graphic[] | undefined = await new Promise((resolve) => {
-            pointerHandle = mapView.on("pointer-move", (event: __esri.ViewPointerMoveEvent) => {
-                void mapView.hitTest(event).then((hitResult: __esri.HitTestResult) => {
-                    const hit: boolean = hitResult.results.filter((result) => result.layer === layer).length > 0;
-                    if (hit) {
-                        mapView.container.style.cursor = "pointer";
-                    } else {
-                        mapView.container.style.cursor = "default";
-                    }
-                    event.stopPropagation();
-
+        try {
+            graphics = await new Promise((resolve) => {
+                pointerHandle = mapView.on("pointer-move", (event: __esri.ViewPointerMoveEvent) => {
+                    void (async () => {
+                        const hitResult = await mapView.hitTest(event, { include: layer, });
+                        highlightHandle?.remove();
+                        const hit = hitResult.results.some((result) => result.layer === layer);
+                        if (hit) {
+                            const graphics = (hitResult.results as __esri.GraphicHit[]).map(x => x.graphic);
+                            highlightHandle = layerView.highlight(graphics);
+                        }
+                    })();
                 });
-            });
-            clickHandle = mapView.on("click", (event: __esri.ViewClickEvent) => {
-                void mapView.hitTest(event).then((hitResult: __esri.HitTestResult) => {
-
-                    const results = hitResult.results.filter((result) => result.layer === layer);
-                    if (results.length > 0) {
-                        if (results[0].type === "graphic") {
-                            pointerHandle.remove();
-                            clickHandle.remove();
-                            mapView.container.ownerDocument?.removeEventListener("keydown", keyDown);
-                            mapView.container.style.cursor = "default";
-                            const graphics = (results as __esri.GraphicHit[]).map(x=> x.graphic)
+                clickHandle = mapView.on("click", (event: __esri.ViewClickEvent) => {
+                    void (async () => {
+                        const hitResult = await mapView.hitTest(event, { include: layer, });
+                        const results = hitResult.results.some((result) => result.layer === layer);
+                        if (results) {
+                            const hitResults = hitResult.results.filter(x => x.type === "graphic" && x.layer === layer);
+                            const graphics = (hitResults as __esri.GraphicHit[]).map(x => x.graphic);
                             resolve(graphics);
                         }
-                    }
+                    })();
+                    void mapView.hitTest(event).then((hitResult: __esri.HitTestResult) => {
+                        const results = hitResult.results.some((result) => result.layer === layer);
+                        if (results) {
+                            const hitResults = hitResult.results.filter(x => x.type === "graphic" && x.layer === layer);
+                            const graphics = (hitResults as __esri.GraphicHit[]).map(x => x.graphic);
+                            resolve(graphics);
+                        }
+                    });
 
                 });
-
+                keyDown = (event: KeyboardEvent) => {
+                    if (event.key === "ESC" || event.key === "Escape") {
+                        resolve(undefined);
+                    }
+                };
+                mapView.container.ownerDocument?.addEventListener("keydown", keyDown);
             });
-            keyDown = (event: KeyboardEvent) => {
-                if (event.key === "ESC" || event.key === "Escape") {
-                    event.stopPropagation();
-                    pointerHandle.remove();
-                    clickHandle.remove();
-                    mapView.container.ownerDocument?.removeEventListener("keydown", keyDown);
-                    mapView.container.style.cursor = "default";
-                    resolve(undefined);
-                }
-            };
-            mapView.container.ownerDocument?.addEventListener("keydown", keyDown);
-        });
-        
+        } finally {
+            pointerHandle?.remove();
+            clickHandle?.remove();
+            highlightHandle?.remove();
+            if (keyDown) {
+                mapView.container.ownerDocument?.removeEventListener("keydown", keyDown);
+            }
+        }
+
         return {
             graphics,
         };
