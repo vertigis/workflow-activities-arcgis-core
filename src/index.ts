ada5cfb4c7de0d636fcc93f6f738bea18f6099db
Colin Doak <cdoak@latitudegeo.com>
2023-02-15 07:59:03 -0800

    fix: - removed selection logic from UpdateSketch - sorted index alphabetically

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { default as AddLayerToMap } from "./activities/AddLayerToMap";
 export { default as CreateLayerFromArcGisServerUrl } from "./activities/CreateLayerFromArcGisServerUrl";
 export { default as CreateLayerFromPortalItem } from "./activities/CreateLayerFromPortalItem";
 export { default as CreateSketch } from "./activities/CreateSketch";
-export { default as UpdateSketch } from "./activities/UpdateSketch";
 export { default as SelectGraphics } from "./activities/SelectGraphics";
+export { default as UpdateSketch } from "./activities/UpdateSketch";
