3a6067a8ea4a2c1bed9f2428c779857e8e8fcbed
Colin Doak <cdoak@latitudegeo.com>
2023-02-16 16:19:09 -0800

    fix: - updated comments and variable names

--- src/activities/CreateMeasurement2D.ts
+++ src/activities/CreateMeasurement2D.ts
@@ -7,32 +7,30 @@ import { MapProvider } from "@geocortex/workflow/runtime/activities/arcgis/MapPr
 import { activate } from "@geocortex/workflow/runtime/Hooks";
 import DistanceMeasurement2D from "@arcgis/core/widgets/DistanceMeasurement2D";
 import AreaMeasurement2D from "@arcgis/core/widgets/AreaMeasurement2D";
-
-
 import MapView from "@arcgis/core/views/MapView";
 
 export interface CreateMeasurement2DInputs {
 
     /**
-     * @description The measurement tool to display.
+     * @description The type of measurement.
      * @required
      */
-    activeTool?: "area" | "distance" | string;    
+    measurementType?: "area" | "distance" | string;
 
     /**
-     * @description Unit system (imperial or metric) or specific unit used for displaying area values.
+     * @description Unit system (imperial or metric) or specific unit used for area values.
      */
     areaUnit?: "metric" | "imperial" | "square-inches" | "square-feet" | "square-us-feet" | "square-yards" | "square-miles" | "square-meters" | "square-kilometers" | "acres" | "ares" | "hectares" | string | undefined;
 
     /**
-     * @description Unit system (imperial or metric) or specific unit used for displaying distance values.
+     * @description Unit system (imperial or metric) or specific unit used for distance values.
      */
     linearUnit?: "metric" | "imperial" | "inches" | "feet" | "us-feet" | "yards" | "miles" | "nautical-miles" | "meters" | "kilometers" | string | undefined;
 
 }
 
 export interface CreateMeasurement2DOutputs {
-    measurement: __esri.AreaMeasurement2DViewModelMeasurement | __esri.DistanceMeasurement2DViewModelMeasurement;
+    measurement: __esri.AreaMeasurement2DViewModelMeasurement | __esri.DistanceMeasurement2DViewModelMeasurement | undefined;
     measurementWidget: any,
 }
 
@@ -50,20 +48,20 @@ export default class CreateMeasurement2D implements IActivityHandler {
         context: IActivityContext,
         type: typeof MapProvider
     ): Promise<CreateMeasurement2DOutputs> {
-        const { linearUnit, activeTool, areaUnit } = inputs;
+        const { linearUnit, measurementType, areaUnit } = inputs;
         const areaUnits = ["metric", "imperial", "square-inches", "square-feet", "square-us-feet", "square-yards", "square-miles", "square-meters", "square-kilometers", "acres", "ares", "hectares"]
-        const linearUnits = ["metric", "imperial", "inches","feet","us-feet", "yards", "miles", "nautical-miles", "meters", "kilometers"]
+        const linearUnits = ["metric", "imperial", "inches", "feet", "us-feet", "yards", "miles", "nautical-miles", "meters", "kilometers"]
         const mapProvider = type.create();
         await mapProvider.load();
         if (!mapProvider.map) {
             throw new Error("map is required");
         }
-        if (!activeTool) {
-            throw new Error("activeTool is required");
+        if (!measurementType) {
+            throw new Error("measurementType is required");
         }
-        if (["area", "distance"].indexOf(activeTool) == -1) {
-            throw new Error(`activeTool not supported: ${activeTool}`);
-        }        
+        if (["area", "distance"].indexOf(measurementType) == -1) {
+            throw new Error(`measurementType not supported: ${measurementType}`);
+        }
         if (linearUnit && linearUnits.indexOf(linearUnit) == -1) {
             throw new Error(`linear unit not supported: ${linearUnit}`);
         }
@@ -71,9 +69,17 @@ export default class CreateMeasurement2D implements IActivityHandler {
             throw new Error(`area unit not supported: ${areaUnit}`);
         }
         const mapView = mapProvider.view as MapView;
+
+        /**
+         * Ideally this would be implemented using AreaMeasurement2DViewModel or DistanceMeasurement2DViewModel
+         * but there is an inconsistency in the Esri Widget View Model pattern with 
+         * measurements that forces the creation of a widget.  A standalone view model 
+         * does not initialize.
+         **/
+
         let measurementWidget;
 
-        switch (activeTool) {
+        switch (measurementType) {
             case "area":
                 measurementWidget = new AreaMeasurement2D({
                     view: mapView,
@@ -87,20 +93,20 @@ export default class CreateMeasurement2D implements IActivityHandler {
                     unit: linearUnit as any,
                 });
                 break;
-
         }
 
         measurementWidget.viewModel.start();
         mapView.container.style.cursor = "crosshair";
-        const measurement: __esri.AreaMeasurement2DViewModelMeasurement | __esri.DistanceMeasurement2DViewModelMeasurement = await new Promise((resolve) => {
-            measurementWidget.watch("viewModel.state", function (state: string) {
-                if (state === "measured") {
-                    resolve(measurementWidget.viewModel.measurement);
-                }
+        const measurement: __esri.AreaMeasurement2DViewModelMeasurement | __esri.DistanceMeasurement2DViewModelMeasurement | undefined
+            = await new Promise((resolve) => {
+                measurementWidget.watch("viewModel.state", function (state: string) {
+                    if (state === "measured") {
+                        resolve(measurementWidget.viewModel.measurement);
+                    }
+                });
+
             });
 
-        });
-        mapView.container.style.cursor = "default";
 
         return {
             measurement,
--- src/activities/CreateMeasurement3D.ts
+++ src/activities/CreateMeasurement3D.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type {
     IActivityHandler,
     IActivityContext,
@@ -11,18 +12,18 @@ import SceneView from "@arcgis/core/views/SceneView";
 /** An interface that defines the inputs of the activity. */
 interface CreateMeasurement3DInputs {
     /**
-     * @description The measurement tool to display.
+     * @description The type of measurement.
      * @required
      */
-    activeTool?: "area" | "direct-line" | string;
+    measurementType?: "area" | "direct-line" | string;
 
     /**
-     * @description Unit system (imperial or metric) or specific unit used for displaying area values.
+     * @description Unit system (imperial or metric) or specific unit used for area values.
      */
     areaUnit?: "metric" | "imperial" | "square-inches" | "square-feet" | "square-us-feet" | "square-yards" | "square-miles" | "square-meters" | "square-kilometers" | "acres" | "ares" | "hectares" | string | undefined;
 
     /**
-     * @description Unit system (imperial or metric) or specific unit used for displaying distance values.
+     * @description Unit system (imperial or metric) or specific unit used for distance values.
      */
     linearUnit?: "metric" | "imperial" | "inches" | "feet" | "us-feet" | "yards" | "miles" | "nautical-miles" | "meters" | "kilometers" | string | undefined;
 
@@ -30,7 +31,7 @@ interface CreateMeasurement3DInputs {
 
 interface CreateMeasurement3DOutputs {
 
-    measurement:  __esri.AreaMeasurement3DViewModelMeasurement | __esri.DirectLineMeasurement3DViewModelMeasurement;
+    measurement: __esri.AreaMeasurement3DViewModelMeasurement | __esri.DirectLineMeasurement3DViewModelMeasurement | undefined;
     measurementWidget: any,
 
 }
@@ -47,20 +48,20 @@ export default class CreateMeasurement3D implements IActivityHandler {
     async execute(inputs: CreateMeasurement3DInputs,
         context: IActivityContext,
         type: typeof MapProvider): Promise<CreateMeasurement3DOutputs> {
-        const { linearUnit, activeTool, areaUnit } = inputs;
+        const { linearUnit, measurementType, areaUnit } = inputs;
         const areaUnits = ["metric", "imperial", "square-inches", "square-feet", "square-us-feet", "square-yards", "square-miles", "square-meters", "square-kilometers", "acres", "ares", "hectares"]
-        const linearUnits = ["metric", "imperial", "inches","feet","us-feet", "yards", "miles", "nautical-miles", "meters", "kilometers"]
+        const linearUnits = ["metric", "imperial", "inches", "feet", "us-feet", "yards", "miles", "nautical-miles", "meters", "kilometers"]
         const mapProvider = type.create();
         await mapProvider.load();
         if (!mapProvider.view) {
             throw new Error("map view is required");
         }
-        if (!activeTool) {
-            throw new Error("activeTool is required");
+        if (!measurementType) {
+            throw new Error("measurementType is required");
         }
-        if (["area", "direct-line"].indexOf(activeTool) == -1) {
-            throw new Error(`activeTool not supported: ${activeTool}`);
-        }        
+        if (["area", "direct-line"].indexOf(measurementType) == -1) {
+            throw new Error(`measurementType not supported: ${measurementType}`);
+        }
         if (linearUnit && linearUnits.indexOf(linearUnit) == -1) {
             throw new Error(`linear unit not supported: ${linearUnit}`);
         }
@@ -68,9 +69,16 @@ export default class CreateMeasurement3D implements IActivityHandler {
             throw new Error(`area unit not supported: ${areaUnit}`);
         }
         const mapView = mapProvider.view as SceneView;
+
+        /**
+         * Ideally this would be implemented using AreaMeasurement3DViewModel or DirectLineMeasurement3DViewModel
+         * but there is an inconsistency in the Esri Widget View Model pattern with 
+         * measurements that forces the creation of a widget.  A standalone view model 
+         * does not initialize.
+         **/        
         let measurementWidget;
 
-        switch (activeTool) {
+        switch (measurementType) {
             case "area":
                 measurementWidget = new AreaMeasurement3D({
                     view: mapView,
@@ -82,23 +90,21 @@ export default class CreateMeasurement3D implements IActivityHandler {
                 measurementWidget = new DirectLineMeasurement3D({
                     view: mapView,
                     unit: linearUnit as any,
-                    
+
                 });
                 break;
-
         }
 
         measurementWidget.viewModel.start();
-        mapView.container.style.cursor = "crosshair";
-        const measurement: __esri.AreaMeasurement3DViewModelMeasurement | __esri.DirectLineMeasurement3DViewModelMeasurement = await new Promise((resolve) => {
-            measurementWidget.watch("viewModel.state", function (state: string) {
-                if (state === "measured") {
-                    resolve(measurementWidget.viewModel.measurement);
-                }
-            });
+        const measurement: __esri.AreaMeasurement3DViewModelMeasurement | __esri.DirectLineMeasurement3DViewModelMeasurement | undefined
+            = await new Promise((resolve) => {
+                measurementWidget.watch("viewModel.state", function (state: string) {
+                    if (state === "measured") {
+                        resolve(measurementWidget.viewModel.measurement);
+                    }
+                });
 
-        });
-        mapView.container.style.cursor = "default";
+            });
         return {
             measurement,
             measurementWidget,
