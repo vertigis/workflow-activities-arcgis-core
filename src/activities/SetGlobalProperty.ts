1cf9a39ba52615db87657037fb5cd1027be0372a
Ryan Cooney <ryan.cooney@vertigis.com>
2023-12-18 10:02:06 -0800

    Change sorting

--- src/activities/SetGlobalProperty.ts
+++ src/activities/SetGlobalProperty.ts
@@ -49,8 +49,8 @@ export default class SetGlobalProperty implements IActivityHandler {
             case "apiKey":
             case "fontsUrl":
             case "kmlServiceUrl":
-            case "geoRSSServiceUrl":
             case "geometryServiceUrl":
+            case "geoRSSServiceUrl":
             case "routeServiceUrl":
                 config[name] = value;
                 break;
