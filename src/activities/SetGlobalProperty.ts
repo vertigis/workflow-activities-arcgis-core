428f93766b919768a09f458cd8ca5065b97af325
Ryan Cooney <rcooney@users.noreply.github.com>
2023-12-19 10:39:51 -0800

    feat: Set Global Property activity (#15)

---
+++ src/activities/SetGlobalProperty.ts
@@ -0,0 +1,64 @@
+import type { IActivityHandler } from "@geocortex/workflow/runtime";
+import config from "@arcgis/core/config";
+
+interface SetGlobalPropertyInputs {
+    /**
+     * @description The name of the global property to modify.
+     * @required
+     * @noExpressions
+     */
+    name:
+        | "apiKey"
+        | "fontsUrl"
+        | "geometryServiceUrl"
+        | "geoRSSServiceUrl"
+        | "kmlServiceUrl"
+        | "routeServiceUrl";
+
+    /**
+     * @description The value to apply to the property.
+     * @required
+     */
+    value: string;
+}
+
+interface SetGlobalPropertyOutputs {}
+
+/**
+ * @category ArcGIS Maps SDK for JavaScript
+ * @clientOnly
+ * @description Configure global properties of the ArcGIS Maps SDK for JavaScript library.
+ * @helpUrl https://developers.arcgis.com/javascript/latest/api-reference/esri-config.html
+ * @supportedApps EXB, GWV
+ */
+export default class SetGlobalProperty implements IActivityHandler {
+    execute(inputs: SetGlobalPropertyInputs): SetGlobalPropertyOutputs {
+        const { name, value } = inputs;
+        if (!name) {
+            throw new Error("name is required");
+        }
+
+        // Ensure the name input only includes a subset of config properties
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+        const prop = name as keyof Pick<
+            config,
+            SetGlobalPropertyInputs["name"]
+        >;
+
+        switch (prop) {
+            case "apiKey":
+            case "fontsUrl":
+            case "geometryServiceUrl":
+            case "geoRSSServiceUrl":
+            case "kmlServiceUrl":
+            case "routeServiceUrl":
+                config[name] = value;
+                break;
+            default:
+                prop satisfies never;
+                throw new Error(`The property '${name}' is not supported`);
+        }
+
+        return {};
+    }
+}
