80cfd2f35d0250ecd23ef02a9517b647030a8198
Ryan Cooney <rcooney@users.noreply.github.com>
2024-01-22 14:00:55 -0800

    Update CreateMediaLayer.ts to use correct import

--- src/activities/CreateMediaLayer.ts
+++ src/activities/CreateMediaLayer.ts
@@ -1,4 +1,4 @@
-import type { IActivityHandler } from "@geocortex/workflow/runtime/IActivityHandler";
+import type { IActivityHandler } from "@vertigis/workflow";
 import MediaLayer from "@arcgis/core/layers/MediaLayer";
 
 interface CreateMediaLayerInputs {
